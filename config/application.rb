8038a0fb8adc779b9186ea50829caa6ecb3533c3
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-18 18:36:06 -0300

    fix: set autoloader files

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module BrokerReportMs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.autoloader = :classic
+    # config.active_job.queue_adapter = :sneakers
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
