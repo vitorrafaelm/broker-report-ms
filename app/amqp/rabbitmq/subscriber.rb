eb71949aa45054241075ced46880d96e5580758d
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-27 23:41:25 -0300

    feat: created subscriber using strategy method

--- app/amqp/rabbitmq/subscriber.rb
+++ app/amqp/rabbitmq/subscriber.rb
@@ -1,17 +1,26 @@
 class Rabbitmq::Subscriber
   def initialize(event_type:, message:)
     @event_type = event_type
-    @payload = message
+    @payload = extract(message: message)
   end
 
   def execute
-    return nil
+    return strategy.execute
   end
 
   private
 
   def strategy
-    return nil
+    strategy = "Rabbitmq::Subscribers::Actions::#{klass_name}".safe_constantize
+    return strategy.new(@payload)
   end
 
+  def extract(message:)
+    extractor = "Rabbitmq::Subscribers::Extractors::#{klass_name}Extractor".safe_constantize
+    return extractor.new(message).extract
+  end
+
+  def klass_name
+    return @event_type.to_s.camelize
+  end
 end
