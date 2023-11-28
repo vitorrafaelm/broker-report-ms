fffc0361977c15b31ce7f51e3f1b44125a089d9d
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-27 23:43:17 -0300

    feat: add broker create event

--- app/amqp/rabbitmq/subscribers/events/broker_created_event.rb
+++ app/amqp/rabbitmq/subscribers/events/broker_created_event.rb
@@ -1,10 +1,16 @@
 class Rabbitmq::Subscribers::Events::BrokerCreatedEvent
   include ::Sneakers::Worker
 
-  from_queue "user_created"
+  from_queue "broker_created"
 
   def work_with_params(payload, delivery_info, metadata)
-    puts payload
+    begin
+      Rabbitmq::Subscribers::Extractors::BrokerCreatedExtractor.new(payload).validate_schema!
+      SubscriberWorker.perform_async('broker_created', payload)
+      return ack!
+    rescue JSON::Schema::ValidationError => error
+      return reject!
+    end
   end
 
 end
