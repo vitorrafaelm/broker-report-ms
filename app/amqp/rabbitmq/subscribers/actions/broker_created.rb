5a9a657cf0ab6ec539db05d177e5740c6a6f6c1e
vitorrafaelm <vitor.rafael1518@gmail.com>
2023-11-27 23:42:30 -0300

    feat: create action thats create a broker

--- app/amqp/rabbitmq/subscribers/actions/broker_created.rb
+++ app/amqp/rabbitmq/subscribers/actions/broker_created.rb
@@ -4,7 +4,28 @@ class Rabbitmq::Subscribers::Actions::BrokerCreated
   end
 
   def execute
-    puts payload
-  end
+    begin
+      broker = {
+        :name => @payload[:name],
+        :identifier => @payload[:broker_identifier],
+        :email => @payload[:email],
+        :password => @payload[:password],
+        :document_type => @payload[:document_type],
+        :document_number => @payload[:document_number],
+        :broker_identifier => @payload[:broker_identifier]
+      }
 
+      Broker.create!(broker)
+      Rails.logger.tagged(self.class) { Rails.logger.info("Successful user created: #{@payload}") }
+    rescue ActiveRecord::RecordInvalid => error
+      if error.record.errors[:identifier]
+        Rails.logger.tagged(error.class) { Rails.logger.error('Invalid Record Error') }
+      end
+    rescue ActiveRecord::RecordNotFound => error
+      Rails.logger.tagged(error.class) { Rails.logger.error(error.message) }
+    rescue StandardError => error
+      Rails.logger.tagged(error.class) { Rails.logger.error(error.message) }
+      raise error
+    end
+  end
 end
